4636bb04e8f8baa4df0000da3fc0e71e14366610
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-07-23 14:32:08 +0430

    Defining and implementing main configuration class

--- osl/run.py
+++ osl/run.py
@@ -7,3 +7,11 @@
 from PIL import Image
 import streamlit as st
 from mrcnn import model, config, visualize
+
+
+class MainConfig(config.Config):
+    """ Model Configuration Class """
+    NAME = "coco_inference"
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 81
